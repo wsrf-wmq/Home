d6807a9e974e6a0621ecf35ddfa9eed8601a962c
rain-sumire <61037890+rain-sumire@users.noreply.github.com>
2020-03-25 22:28:46 +0800

    Update proto.c

--- proto.c
+++ proto.c
@@ -2,8 +2,8 @@
 int imax(int n,int m);
 int main()
 {
-  printf("The maxinum of %d and %d is %d\n",3,5,imax(3));
-  printf("The maxinum of %d and %d is %d\n",3,5,imax(5));
+  printf("The maxinum of %d and %d is %d\n",3,5,imax(3,5));
+  printf("The maxinum of %d and %d is %d\n",3,5,imax(3.0,5.0));
 }
 int imax(int n,int m)
 {
